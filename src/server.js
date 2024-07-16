cf72dbd186b9da035f0f992bacf59abd244f5a7d
phampham13 <phamthikimngan.1013@gmail.com>
2024-07-16 16:49:43 +0700

    fix cor

--- src/server.js
+++ src/server.js
@@ -14,7 +14,20 @@ import { env } from '~/config/environment'
 const START_SERVER = () => {
   const app = express()
   //app.use(cors())
-  app.use(cors({ credentials: true, origin: 'http://localhost:5173' })); //origin:true
+  const allowedOrigins = ['http://localhost:5173', 'https://dfreebook.vercel.app'];
+
+  const corsOptions = {
+    origin: function (origin, callback) {
+      if (allowedOrigins.includes(origin) || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
+  };
+
+  app.use(cors(corsOptions));
   app.use(bodyParser.json())
   app.use(cookieParser())
   app.use(express.json())
@@ -29,13 +42,14 @@ const START_SERVER = () => {
 
   require('./utils/scheduler');
 
+  //support Render.com
   if (process.env.BUILD_MODE === "dev") {
     app.listen(env.APP_PORT, env.APP_HOST, () => {
       console.log(`Hello ${env.AUTHOR}, I am running at ${env.APP_HOST}:${env.APP_PORT}/`)
     })
   } else {
     app.listen(process.env.PORT, () => {
-      console.log(`Hello ${env.AUTHOR}, I am running at ${process.env.PORT}`)
+      console.log(`production: Hello ${env.AUTHOR}, I am running at ${process.env.PORT}`)
     })
 
   }
@@ -50,4 +64,3 @@ CONNECT_DB()
     console.error(error)
     process.exit(0)
   })
-
